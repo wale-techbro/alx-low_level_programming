54bcbe7ed6c2128659f63e6045835c0ad3f66c92
David Olayiwola <hello.davidolawale@gmail.com>
2022-12-14 15:16:44 -0800

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,6 +23,7 @@ _putchar(' ');
 _putchar((c % 10) + '0');
 }
 if (b < 9)
+{
 _putchar(',');
 _putchar(' ');
 }
@@ -31,7 +32,7 @@ else
 {
 _putchar((c / 10) + '0');
 _putchar((c % 10) + '0');
-if (b >9)
+if (b > 9)
 {
 _putchar(',');
 _putchar(' ');
